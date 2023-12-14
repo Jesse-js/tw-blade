55802ef8b30956887f8c860a14dcc7546207e707
Jesse-js <jesse-jjs@hotmail.com>
2023-12-13 23:51:44 -0300

    feat: loops blade structure

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('index', [
+        'projects' => [
+            [
+                'image' => 'cabin.png'
+            ],
+            [
+                'image' => 'cake.png'
+            ],
+            [
+                'image' => 'circus.png'
+            ],
+            [
+                'image' => 'game.png'
+            ],
+            [
+                'image' => 'safe.png'
+            ],
+            [
+                'image' => 'submarine.png'
+            ]
+        ]
+    ]);
 });
 
 Route::get('/data', function () {
