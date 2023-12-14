29bb33a90b733f83c150df6eaf24bed957561a74
Jesse-js <jesse-jjs@hotmail.com>
2023-12-14 00:06:43 -0300

    feat: loop variable blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -76,7 +76,7 @@
                 @forelse ($projects as $project)    
                     @continue(!$project['active'])
                     <div class="col-md-6 col-lg-4 mb-5">
-                        <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
+                        <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal{{ $loop->iteration }}">
                             <div
                                 class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                                 <div class="portfolio-item-caption-content text-center text-white"><i
