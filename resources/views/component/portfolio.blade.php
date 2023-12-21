b70d21c16ed9ebeff81c95cdc30627d91ca97a41
Jesse-js <jesse-jjs@hotmail.com>
2023-12-20 22:31:34 -0300

    feat: social item component

--- resources/views/component/portfolio.blade.php
+++ resources/views/component/portfolio.blade.php
@@ -100,14 +100,19 @@
                 <!-- Footer Social Icons-->
                 <div class="col-lg-4 mb-5 mb-lg-0">
                     <h4 class="text-uppercase mb-4">Around the Web</h4>
-                    <a class="btn btn-outline-light btn-social mx-1" href="#!"><i
-                            class="fab fa-fw fa-facebook-f"></i></a>
-                    <a class="btn btn-outline-light btn-social mx-1" href="#!"><i
-                            class="fab fa-fw fa-twitter"></i></a>
-                    <a class="btn btn-outline-light btn-social mx-1" href="#!"><i
-                            class="fab fa-fw fa-linkedin-in"></i></a>
-                    <a class="btn btn-outline-light btn-social mx-1" href="#!"><i
-                            class="fab fa-fw fa-dribbble"></i></a>
+
+                    <x-social-item link="facebook">
+                        <i class="fab fa-fw fa-facebook-f"></i>
+                    </x-social-item>
+                    <x-social-item link="twitter">
+                        <i class="fab fa-fw fa-twitter"></i>
+                    </x-social-item>
+                    <x-social-item link="linkedin">
+                        <i class="fab fa-fw fa-linkedin-in"></i>
+                    </x-social-item>
+                    <x-social-item link="dribbble">
+                        <i class="fab fa-fw fa-dribbble"></i>
+                    </x-social-item>
                 </div>
                 <!-- Footer About Text-->
                 <div class="col-lg-4">
