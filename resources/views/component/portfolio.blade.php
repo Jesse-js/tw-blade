8ab48a0fca97780723a5e079474991f04e1be70a
Jesse-js <jesse-jjs@hotmail.com>
2023-12-28 19:16:15 -0300

    feat: add footer section component

--- resources/views/component/portfolio.blade.php
+++ resources/views/component/portfolio.blade.php
@@ -89,18 +89,23 @@
         <div class="container">
             <div class="row">
                 <!-- Footer Location-->
-                <div class="col-lg-4 mb-5 mb-lg-0">
-                    <h4 class="text-uppercase mb-4">Location</h4>
+                <x-footer-section>
+                    <x-slot name="title">
+                        <h4 class="text-uppercase mb-4">Location</h4>
+                    </x-slot>
+                    
                     <p class="lead mb-0">
                         2215 John Daniel Drive
                         <br />
                         Clark, MO 65243
                     </p>
-                </div>
+                </x-footer-section>
                 <!-- Footer Social Icons-->
-                <div class="col-lg-4 mb-5 mb-lg-0">
-                    <h4 class="text-uppercase mb-4">Around the Web</h4>
-
+                <x-footer-section>
+                    <x-slot name="title">
+                        <h4 class="text-uppercase mb-4">Around the Web</h4>
+                    </x-slot>
+                    
                     <x-social-item link="facebook">
                         <i class="fab fa-fw fa-facebook-f"></i>
                     </x-social-item>
@@ -113,16 +118,19 @@
                     <x-social-item link="dribbble">
                         <i class="fab fa-fw fa-dribbble"></i>
                     </x-social-item>
-                </div>
+                </x-footer-section>
                 <!-- Footer About Text-->
-                <div class="col-lg-4">
-                    <h4 class="text-uppercase mb-4">About Freelancer</h4>
+                <x-footer-section>
+                    <x-slot name="title">
+                        <h4 class="text-uppercase mb-4">About Freelancer</h4>
+                    </x-slot>
+                    
                     <p class="lead mb-0">
                         Freelance is a free to use, MIT licensed Bootstrap theme created by
                         <a href="http://startbootstrap.com">Start Bootstrap</a>
                         .
                     </p>
-                </div>
+                </x-footer-section>
             </div>
         </div>
     </footer>
